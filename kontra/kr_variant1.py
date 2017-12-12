a6b836e26e838437066e5b381bd96effd1fe9ff7
AnnMokhova <31881973+AnnMokhova@users.noreply.github.com>
2017-12-12 13:08:55 +0300

    Add files via upload

--- kontra/kr_variant1.py
+++ kontra/kr_variant1.py
@@ -9,6 +9,18 @@ with open ('Extinct_languages.tsv', 'r', encoding='utf-8') as f:
         name, num, definit = line.split('/t')
         if definit == 'Critically endangered':
             summ += summ
-            print('число языков со статусом ', summ)
-            
-            
+    print('число языков со статусом ', summ)
+    #задание3
+    lang = input('введите название языка ')
+    l_list = []
+    l_list.append(lang)
+    while lang != '':
+        lang = input('введите название языка ')
+        l_list.append(lang)
+    name = line.split(' ')
+    for lang in l_list:
+        for name in line:
+            if lang == name:
+                print(line)
+            else:
+                print('название не встретилось в списке')
