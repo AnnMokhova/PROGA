bd61db70f1971512f96ea7683d90b40d7abec4d5
AnnMokhova <31881973+AnnMokhova@users.noreply.github.com>
2018-01-11 17:46:08 +0300

    Delete 222.py

--- seminars/222.py
+++
@@ -1,8 +0,0 @@
-def open_text():
-    with open ('text.txt', 'r', encoding='utf-8') as f:
-        text = f.read()
-        lines = text.splitlines()
-        words = lines.strip(' ')
-        print(lines)
-
-open_text()
