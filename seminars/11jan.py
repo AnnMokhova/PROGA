a5a36a3919e61db064b2de86ca0dab8337694e06
AnnMokhova <31881973+AnnMokhova@users.noreply.github.com>
2018-01-11 17:58:19 +0300

    Add files via upload

---
+++ seminars/11jan.py
@@ -0,0 +1,14 @@
+#import random #random - модуль по умолчанию
+#random.random() #сначала имя модуля, потом имя функции
+
+#def f1():
+    return 1
+
+#import f
+#print(f.f1())
+
+#random.randit(1,5) #случайное число в диапозоне
+
+#a = [1, 2, 3, 4, 5, 6]
+#random.shuffle(a)
+
